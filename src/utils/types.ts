010e8da83f9b73b5930b9c5e75a7bec99e0904e1
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-11-15 18:14:13 +0200

    display meal searchedByName

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,13 +2,21 @@ export interface RootState {
 
 }
 
+// export interface IState {
+//     searchedMeals: [];
+// }
+
 export interface IState {
-    searchedMeals: [];
-}
+    searchedMeals: IMeal[];
+  }
   
 export interface IMeal {
     idIngredient: string,
     strDescription: string,
     strIngredient: string,
-    strType: null | string
+    strMealThumb: string,
+    strType: undefined | string,
+    strYoutube: string | undefined,
+    idMeal: undefined | string,
+    strMeal: string
 }
--- src/views/SearchByName.vue
+++ src/views/SearchByName.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 
-import {computed, ref} from 'vue';
+import {computed, onMounted, ref} from 'vue';
 import axiosClient from '../utils/axiosClient';
 import store from '../store';
+import { useRoute } from 'vue-router';
 import { IState } from '../utils/types';
 
+const route = useRoute();
 const keyword = ref('');
 const meals = computed(() => (store.state as IState).searchedMeals);
 
@@ -14,6 +16,10 @@ const searchMeals = () => {
     axiosClient.get(`seaerch.php?s=${keyword.value}`);
     store.dispatch('searchMeals', keyword.value)    
 }
+
+onMounted(() => {
+    keyword.value = route.params.name as string;
+})
 </script>
 
 <template>
@@ -27,7 +33,15 @@ const searchMeals = () => {
         />
     </div>
 
-    <div>
+    <div class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-5">
+        <div v-for="meal of meals" :key="meal.idMeal">
+            <img :src="meal.strMealThumb" :alt="meal.strMeal"/>
+            <h3>{{ meal.strMeal }}</h3>
+            <div>
+                <a :href="meal.strYoutube" target="_blank">YouTube</a>
+                <router-link to="/">View</router-link>
+            </div>
+        </div>
         <pre>
             {{ meals }}
         </pre>
